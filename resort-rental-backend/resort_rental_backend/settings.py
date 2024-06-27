ce32b796e58f53072d2c614ceaac0b6179054e8f
Hadayetullah <hadayetullah30@gmail.com>
2024-06-27 18:58:09 +0600

    Create a new app app_property and create Property model

--- resort-rental-backend/resort_rental_backend/settings.py
+++ resort-rental-backend/resort_rental_backend/settings.py
@@ -85,13 +85,14 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
 
-    'dj_rest_auth'
+    'dj_rest_auth',
     'dj_rest_auth.registration',
 
     'corsheaders',
 
 
     'app_useraccount',
+    'app_property',
 ]
 
 MIDDLEWARE = [
@@ -176,6 +177,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
