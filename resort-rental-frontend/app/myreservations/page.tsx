28feed483705752a7a31f187f7ecd46324b902c6
Hadayetullah <hadayetullah30@gmail.com>
2024-09-04 20:53:50 +0600

    Make myreservations page dynamic

--- resort-rental-frontend/app/myreservations/page.tsx
+++ resort-rental-frontend/app/myreservations/page.tsx
@@ -1,82 +1,59 @@
 import ContactButton from "@/app/components/ContactButton";
 import PropertyList from "@/app/components/properties/PropertyList";
 import Image from "next/image";
+import apiService from "../services/apiService";
+import Link from "next/link";
 
-const MyReservationPage = () => {
+const MyReservationPage = async () => {
+  const reservations = await apiService.get("/api/auth/myreservations/");
+
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
-      <h1 className="my-6 text-2xl">MyReservationPage</h1>
+      <h1 className="my-6 text-2xl">My reservations</h1>
 
       <div className="space-y-4">
-        <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
-          <div className="col-span-1">
-            <div className="relative overflow-hidden aspect-square rounded-xl">
-              <Image
-                fill
-                src={"/beach_1.jpg"}
-                alt="Beach house"
-                className="hover:scale-110 object-cover transition h-full w-full"
-              />
+        {reservations.map((reservation: any) => {
+          return (
+            <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
+              <div className="col-span-1">
+                <div className="relative overflow-hidden aspect-square rounded-xl">
+                  <Image
+                    fill
+                    src={reservation.property.image_url}
+                    alt="Beach house"
+                    className="hover:scale-110 object-cover transition h-full w-full"
+                  />
+                </div>
+              </div>
+
+              <div className="col-span-1 md:col-span-3">
+                <h2 className="mb-4 text-xl">{reservation.property.title}</h2>
+
+                <p className="mb-2">
+                  <strong>Check in date:</strong> {reservation.start_date}
+                </p>
+                <p className="mb-2">
+                  <strong>Check out date:</strong> {reservation.end_date}
+                </p>
+
+                <p className="mb-2">
+                  <strong>Number of nights:</strong>{" "}
+                  {reservation.number_of_nights}
+                </p>
+                <p className="mb-2">
+                  <strong>Total price:</strong> {reservation.total_price}
+                </p>
+
+                <Link
+                  href={`/properties/${reservation.property.id}`}
+                  className="mt-6 cursor-pointer inline-block py-4 px-6 bg-primary text-white rounded-xl"
+                >
+                  Go to property
+                </Link>
+              </div>
             </div>
-          </div>
-
-          <div className="col-span-1 md:col-span-3">
-            <h2 className="mb-4 text-xl">Property name</h2>
-
-            <p className="mb-2">
-              <strong>Check in date:</strong> 08/05/24
-            </p>
-            <p className="mb-2">
-              <strong>Check out date:</strong> 10/05/24
-            </p>
-
-            <p className="mb-2">
-              <strong>Number of nights:</strong> 2
-            </p>
-            <p className="mb-2">
-              <strong>Total price:</strong> 200
-            </p>
-
-            <div className="mt-6 cursor-pointer inline-block py-4 px-6 bg-primary text-white rounded-xl">
-              Go to property
-            </div>
-          </div>
-        </div>
-
-        <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
-          <div className="col-span-1">
-            <div className="relative overflow-hidden aspect-square rounded-xl">
-              <Image
-                fill
-                src={"/beach_1.jpg"}
-                alt="Beach house"
-                className="hover:scale-110 object-cover transition h-full w-full"
-              />
-            </div>
-          </div>
-
-          <div className="col-span-1 md:col-span-3">
-            <h2 className="mb-4 text-xl">Property name</h2>
-
-            <p className="mb-2">
-              <strong>Check in date:</strong> 08/05/24
-            </p>
-            <p className="mb-2">
-              <strong>Check out date:</strong> 10/05/24
-            </p>
-
-            <p className="mb-2">
-              <strong>Number of nights:</strong> 2
-            </p>
-            <p className="mb-2">
-              <strong>Total price:</strong> 200
-            </p>
-
-            <div className="mt-6 cursor-pointer inline-block py-4 px-6 bg-primary text-white rounded-xl">
-              Go to property
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </main>
   );
