91e10973454d5d44d83066c91047c00ced473375
Hadayetullah <hadayetullah30@gmail.com>
2024-09-24 17:38:39 +0600

    Configure asgi.py file initially

--- resort-rental-backend/resort_rental_backend/asgi.py
+++ resort-rental-backend/resort_rental_backend/asgi.py
@@ -9,8 +9,34 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resort_rental_backend.settings')
 
 application = get_asgi_application()
+
+
+# Below part will be commented after creating routing and TokenAuthMiddleware
+# django_asgi_app = get_asgi_application()
+
+
+'''
+from app_chat import routing
+from app_chat.token_auth import TokenAuthMiddleware
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": TokenAuthMiddleware(
+            URLRouter(routing.websocket_urlpatterns)
+        ),
+    }
+)
+'''
