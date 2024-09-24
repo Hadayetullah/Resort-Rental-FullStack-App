b75cc715e682c023988d0848ea0ebbf5e3999863
Hadayetullah <hadayetullah30@gmail.com>
2024-09-24 17:54:28 +0600

    Create routing.py file

---
+++ resort-rental-backend/app_chat/routing.py
@@ -0,0 +1,8 @@
+from django.urls import re_path
+
+# Below part will be commented after creating consumers
+# from . import consumers
+
+# websocket_urlpatterns = [
+#     re_path("ws/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
+# ]
