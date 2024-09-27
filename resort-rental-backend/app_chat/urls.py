a532eaff3d58c97a1a0476af39afaee09eb33418
Hadayetullah <hadayetullah30@gmail.com>
2024-09-27 23:32:42 +0600

    Create api endpoint for conversations_detail

--- resort-rental-backend/app_chat/urls.py
+++ resort-rental-backend/app_chat/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.conversations_list, name='api_conversation_list'),
+    path('<uuid:pk>/', views.conversations_detail, name='api_conversations_detail'),
 ]
