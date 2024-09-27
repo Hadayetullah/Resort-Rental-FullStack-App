76b79e15b33ed22f45c02865222cb283b98b4b11
Hadayetullah <hadayetullah30@gmail.com>
2024-09-27 23:05:29 +0600

    Install some packages and add authentication in ConversationPage

--- resort-rental-frontend/app/inbox/[id]/page.tsx
+++ resort-rental-frontend/app/inbox/[id]/page.tsx
@@ -1,6 +1,17 @@
 import ConversationDetail from "@/app/components/inbox/ConversationDetail";
+import { getUserId } from "@/app/lib/actions";
 
-const ConversationPage = () => {
+const ConversationPage = async () => {
+  const userId = await getUserId();
+
+  if (!userId) {
+    return (
+      <main className="max-w-[1500px] mx-auto px-6 py-12">
+        <p>You need to be authenticated...</p>
+      </main>
+    );
+  }
+
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
       <ConversationDetail />
