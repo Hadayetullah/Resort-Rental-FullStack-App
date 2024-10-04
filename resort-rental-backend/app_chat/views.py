c407d6527366f4954048ad45b9a8e3dd7d6f0867
Hadayetullah <hadayetullah30@gmail.com>
2024-10-04 22:23:12 +0600

    Create ConversatioMessageSerializer and modify conversation_list to be availabe chat messages to the frontend

--- resort-rental-backend/app_chat/views.py
+++ resort-rental-backend/app_chat/views.py
@@ -2,8 +2,8 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 
-from .models import Conversation
-from .serializers import ConversationListSerializer, ConversationDetailSerializer
+from .models import Conversation, ConversationMessage
+from .serializers import ConversationListSerializer, ConversationDetailSerializer, ConversationMessageSerializer
 
 
 # Create your views here.
@@ -19,7 +19,10 @@ def conversations_detail(request, pk):
     conversation = request.user.conversations.get(pk=pk)
 
     conversation_serializer = ConversationDetailSerializer(conversation, many=False)
+    message_serializer = ConversationMessageSerializer(conversation.messages.all(), many=True)
+
 
     return JsonResponse({
-        'conversation': conversation_serializer.data
+        'conversation': conversation_serializer.data,
+        'messages': message_serializer.data
     }, safe=False)
