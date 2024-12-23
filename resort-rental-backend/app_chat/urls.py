570832c136a875b78c42f2d32adc47bcb2f83e1d
Hadayetullah <hadayetullah30@gmail.com>
2024-12-23 20:01:30 +0600

    Create api_conversations_start

--- resort-rental-backend/app_chat/urls.py
+++ resort-rental-backend/app_chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.conversations_list, name='api_conversation_list'),
+    path('start/<uuid:user_id>/', views.conversations_start, name='api_conversations_start'),
     path('<uuid:pk>/', views.conversations_detail, name='api_conversations_detail'),
 ]
--- resort-rental-backend/app_chat/views.py
+++ resort-rental-backend/app_chat/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 
 from .models import Conversation, ConversationMessage
 from .serializers import ConversationListSerializer, ConversationDetailSerializer, ConversationMessageSerializer
+from app_useraccount.models import User
 
 
 # Create your views here.
@@ -26,3 +27,28 @@ def conversations_detail(request, pk):
         'conversation': conversation_serializer.data,
         'messages': message_serializer.data
     }, safe=False)
+
+
+
+@api_view(['GET'])
+def conversations_start(request, user_id):
+    # conversations = Conversation.objects.filter(users__in=[user_id]).filter(users__in=[request.user.id])
+    conversations = Conversation.objects.filter(users__in=[request.user.id, user_id])
+    if conversations.count() > 0:
+        conversation = conversations.first()
+        return JsonResponse({
+            'success': True,
+            'conversation_id': conversation.id
+        })
+    else:
+        user = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user, user)
+        # conversation.users.add(request.user)
+        # conversation.users.add(user)
+        # conversation.save()
+        return JsonResponse({
+            'success': True,
+            'conversation_id': conversation.id
+        })
+    
